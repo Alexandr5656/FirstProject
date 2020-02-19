3bea0509c32c8ab18234f680e2fe35f38e07b49c
Alexandr5656 <49129862+Alexandr5656@users.noreply.github.com>
2020-02-19 14:27:54 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foods
+{
+    public abstract class Resturant
+    {
+        public string name;
+        public double price;
+        public int ratings;
+        public string type;
+        public string location;
+        public int openHour;
+        public int closeHour;
+        public int[] busyHours;
+
+        public string Hours {
+            get {
+                return (openHour + "-" + closeHour);
+            }
+            
+        }
+       
+
+
+    }
+    
+}
